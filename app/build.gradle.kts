95e3dd3e42e19d6d65555345f8842aac1e595906
Low <54532616+Lowkh@users.noreply.github.com>
2025-12-09 13:04:20 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("com.google.devtools.ksp")
+    id("kotlin-android")
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.0"  // ktlint Gradle plugin
 
 }
 
